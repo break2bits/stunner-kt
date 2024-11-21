132bc000d792e4f2e7530f0d42cdf0e6cf885a49
Hal Rogers <hal.c.rogers@gmail.com>
2024-11-21 14:14:25 -0500

    add default port

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import com.hal.stunner.cli.CliArgumentParser
 
 fun main(args: Array<String>) {
     val parser = CliArgumentParser(args)
-    val port = parser.getArg<Int>("p", "port")
+    val port = parser.getArg<Int>("p", "port", 3478)
     val protocol = parser.getArg<String>("s", "scheme", "udp")
     println("port: $port")
     println("protocol: $protocol")
