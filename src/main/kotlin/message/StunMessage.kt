65b380f6fc435dec3b2549f1d5b9fb47e87ec960
Hal Rogers <hal.c.rogers@gmail.com>
2024-11-21 17:01:50 -0500

    whitespace

--- src/main/kotlin/message/StunMessage.kt
+++ src/main/kotlin/message/StunMessage.kt
@@ -1,12 +1,11 @@
 package com.hal.stunner.message
 
-
 class StunMessage(private val stunHeader: StunHeader) {
     companion object {
 
         fun fromBytes(bytes: ByteArray) {
             val header = StunHeader.fromBytes(bytes)
-            // parse attributes
+            // parse attributes using length from header
         }
 
     }
