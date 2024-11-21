4a34442b3a673c8cb7ce9c0de4b549eeda4f01cf
Hal Rogers <hal.c.rogers@gmail.com>
2024-11-21 13:58:29 -0500

    remove println

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,6 @@ package com.hal.stunner
 import com.hal.stunner.cli.CliArgumentParser
 
 fun main(args: Array<String>) {
-    println("Ran stunner")
-
     val parser = CliArgumentParser(args)
     val port = parser.getArg<Int>("p", "port")
     println("port: $port")
